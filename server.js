e5f40a57077e206c8af8a30432865061a54b5bdc
Jéssica Carpim Ambar <jecarpim@gmail.com>
2019-12-01 00:39:08 -0300

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ app.get('/.', (reg, res) => {
   res.status(200).send({message: 'Olá mundo estou funcionando!'});
 })
 
+app.get('/ola', (reg, res) => {
+  res.status(200).send({message: 'Oi eu sou a rota Ola!'});
+})
+
 app.listen (3001, () => {
   console.log('Api rodando na porta 3001');
 })
